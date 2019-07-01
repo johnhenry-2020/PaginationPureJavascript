afd122e74f2f88e2a6a6f8c8b8414b3a24f384df
JJ <jonjolivette777@aol.com>
2019-07-01 10:56:24 -0500

    addition of display page functionality/logic

--- js/script.js
+++ js/script.js
@@ -2,63 +2,19 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
+/* DOMContentLoaded...
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+I understand that there is a "best practice" method to the madness as it relates every single aspect of a program's
+development which would lead one to naturally situate the code at the base of the html's body but I also have come
+to understand that great products are developed via teamwork and collaboration so with all that said I believe it
+best to wrap all my code from here forward in a DOMContentLoaded event handler so no matter the preference of those
+involved regarding the placement of the javascipt files in the html's head or body tags none of the javascript will
+fires until the initial HTML document has been completely loaded and parsed. Also regardless as to the connectivity
+of the user or should the javascript be turned off on a browser the page's main content will atleast begin to load
+without the user having to sit there waiting for the stylesheets, images, and subframes to load b/c of some hangup
+involving the javascript. All of this is to provide a very thorough explanation for the utilization of the following
+event listener not only here but in all the program's I'm solely responsible for or collaborate on moving forward...*/
 
-
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
-
-
-
-
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
-
-
-
-
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
-
-//I understand that there is a "best practice" method to the madness as it relates every single aspect of a program's
-//development which would lead one to naturally situate the code at the base of the html's body but I also have come
-//to understand that great products are developed via teamwork and collaboration so with all that said I believe it
-//best to wrap all my code from here forward in a DOMContentLoaded event handler so no matter the preference of those
-//involved regarding the placement of the javascipt files in the html's head or body tags none of the javascript will
-//fires until the initial HTML document has been completely loaded and parsed. Also regardless as to the connectivity
-//of the user or should the javascript be turned off on a browser the page's main content will atleast begin to load
-//without the user having to sit there waiting for the stylesheets, images, and subframes to load b/c of some hangup
-//involving the javascript. All of this is to provide a very thorough explanation for the utilization of the following
-//event listener not only here but in all the program's I'm solely responsible for or collaborate on moving forward...
 window.addEventListener('DOMContentLoaded', (event) => {
 
    /*================
@@ -67,10 +23,35 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
    // "studentList" + "student" variables store the student list item elements in the student list...
    const studentList = document.querySelector('.student-list');
+   // variable stores the number of items to show on each “page”, which for this program, is 10...
    const students = document.querySelectorAll(".student-item");
+   // "itemsPerpage" variable stores the desired items to be shown on each page...
+   const itemsPerPage = 10;
 
-   // variable stores the number of items to show on each “page”, which for this project, is 10...
-   let showStudents;
+   /*================================
+    logic for displaying page(s)...
+   ================================*/
 
+   // "show10Students" function serves to show up to 10 of the students we want displayed on a given page...
+   // "list" parameter is the list of students that you’ll pass in...
+   // "page" parameter represents the page number...
+   const show10Students = (list, page) => {
+      // Start Index of the list items  = (page parameter * items per page) - items per page
+      let start = (page * itemsPerPage) - itemsPerPage;
+      // End Index of the list items = page parameter * items per page
+      let end = page * itemsPerPage;
+      // LOOPING over the list parameter which serves to display any list item
+      // with an INDEX that is GREATER THAN OR EQUAL to the start index variable and LESS THAN the end index variable...
+      for (var i = 0; i < students.length; i++) {
+         if (i >= start || i < end) {
+            students[i].style.display = "none";
+         } else {
+            students[i].style.display = "";
+         }
+      }
+   }
+
+   show10Students(studentList, "");
+   // sanity check
    console.log('All The DOM Content has load and you\'re ready to go!');
 }); //conclusion of DOMContentLoaded function
