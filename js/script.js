859a1539ac20b1ec3c03e058c81380e79a1cb005
JJ <jonjolivette777@aol.com>
2019-07-08 12:09:06 -0500

    added user search bar input feedback functionality

--- js/script.js
+++ js/script.js
@@ -17,9 +17,14 @@ event listener not only here but in all the program's I'm solely responsible for
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
-   /*================
+   // provides DOM reference point while in development...
+   window.onclick = (e) => {
+      console.log(e.target);
+      console.log(e.target.tagName);
+   }
+   /*==============================
    declaration of global variables
-   ================*/
+   ===============================*/
 
    // "studentList" + "student" variables store the student list item elements in the student list...
    const entireStudentList = document.querySelector('.student-list');
@@ -27,9 +32,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
    const eachStudentItem = document.querySelectorAll(".student-item");
    // "itemsPerpage" variable stores the desired items to be shown on each page...
    const itemsPerPage = 10;
+   let i;
 
    /*================================
-    logic for displaying page(s)...
+    logic for displaying "page(s)"...
    ================================*/
 
    // "show10Students" function serves to show up to 10 of the students we want displayed on a given page...
@@ -42,7 +48,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
       let end = page * itemsPerPage;
       // LOOPING over the list parameter which serves to display any list item
       // with an INDEX that is LESS THAN OR EQUAL to the start index variable and GREATER THAN the end index variable...
-      for (var i = 0; i < eachStudentItem.length; i++) {
+      for (i = 0; i < eachStudentItem.length; i++) {
          if (i < start || i >= end) {
             eachStudentItem[i].style.display = "none";
          } else {
@@ -52,11 +58,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
    }
 
    /*================================
-   logic for pagination link(s)...
+      logic for pagination link(s)...
    ================================*/
    // function creates and appends functioning pagination links...
    // accepts a single list parameter to represent the actual list of students passed in as an argument...
-
 
 
    // You can divide list.length by the max number of items per page to figure out how many pages are needed,
@@ -67,31 +72,61 @@ window.addEventListener('DOMContentLoaded', (event) => {
    paginationDiv.style.textAlign = 'center';
    paginationDiv.style.margin = '0, auto';
 
+
    const createPagLinks = (list) => {
-      for (var i = 0; i < maxNumPages; i++) {
+      //creates unordered list element
+      const ul = document.createElement('ul');
+      // loops through every index in list allowing access to each students/objects via maxNumPages variable
+      for (i = 0; i < maxNumPages; i++) {
          // https://teamtreehouse.com/community/difference-between-classlist-and-classname
+         // for each student/object in maxNumPages array the class of "pagination" is added to the newly formed pagination div
          paginationDiv.classList.add("pagination");
+         // with the div created & loaded with the class of "pagination" we append that div to the list of students so it's now properly situated on the DOM
          entireStudentList.appendChild(paginationDiv);
-         const ul = document.createElement('ul');
+         // append an unordered list to the pagaination div
          paginationDiv.appendChild(ul);
+         // create the list items for the newly formed unordered list
          const li = document.createElement('li');
+         // situate the newly formed list items directly in the unordered list by appending them to the 'ul'
          ul.appendChild(li);
+         // create the pagination anchor tags
          const link = document.createElement('a');
-         li.setAttribute("className", "active");
+         // obtain access to the first list item in the unordered list
+         let pagLinks = document.querySelector('a');
+         const firstLi = ul.firstChild;
+         // set the the value of "a" to the value of that of the first anchor tag
+         const a = firstLi.firstChild;
+         // // give pagination link's class the class name of "active"
+         // firstLi.setAttribute("className", "active");
+         //anchor tags become part of each list item
          li.appendChild(link);
+         //the list item's anchor tags have their attributes set to where they are loaded with active links
          link.setAttribute("href", "#");
          // https://stackoverflow.com/questions/3496280/set-text-of-anchor-tag-in-javascript
          link.innerHTML = i + 1;
-         link.addEventListener('click', () => {
-            let pagLinks = document.querySelector('a');
-            for (let i = 0; i < maxNumPages.length; i++) {
-               pagLinks[i].className = "";
+         // when the pagination links are clicked we are setting the "paglinks" variable up to obtain
+         // access to the anchor tags that represent each pagination page holding a maximum of 10 studen objects per "page"
+         link.addEventListener('click', (e) => {
+            // for (i = 0; i < maxNumPages.length; i++) {
+            // pagLinks[i].className = "";
+            if (e.target.classList != "active") {
+               e.target.style.backgroundColor = "#3d474f";
+            } else if (e.target.classList == "active") {
+               e.target.backgroundColor = "transparent";
+               //anchor tag is clicked the class link's class is set to active
             }
-            event.target.className = "active";
             show10Students(entireStudentList, link.innerHTML);
          });
       }
    }
+
+
+
+
+
+
+
+
 
 
    // CALLING show10Students FUNCTION...
@@ -99,16 +134,133 @@ window.addEventListener('DOMContentLoaded', (event) => {
    createPagLinks(entireStudentList);
 
 
-   /*================================
+   /*========================
       treehouse logo node...
-      ================================*/
+   ======================c===*/
    // https://stackoverflow.com/questions/7932759/dom-appendchild-to-insert-images
    //"header" variable provides access to h2 - STUDENTS header
    // const page = document.querySelector(".page");
-   const header = document.querySelector("h2");
-   const logoImg = document.createElement('img');
-   logoImg.src = "static/treehouse-logo-landscape.png";
-   const logo = header.appendChild(logoImg, header);
+   // const header = document.querySelector("h2");
+   // const logoImg = document.createElement('img');
+   // logoImg.src = "static/treehouse-logo-landscape.png";
+   // header.appendChild(logoImg, header);
+
+
+
+   /*======================================
+      search bar functionality/animation
+   ========================================*/
+   const body = document.querySelector('body');
+   const searchBar = document.querySelector('#search');
+   const searchIcon = document.querySelector('.search-icon');
+   // "searchInput" variable affords you access to the search input field
+   const searchInput = document.querySelector('#search-input');
+   let active = false;
+
+
+   body.addEventListener('click', (e) => {
+      if (e.target.id === 'search' || e.target.id === 'search-input' || e.target.className === 'search-icon') {
+         if (!active) {
+            searchBar.classList.add('active');
+            searchIcon.classList.add('active');
+            searchInput.classList.add('active');
+            active = true;
+         }
+      } else if (active) {
+         searchBar.classList.remove('active');
+         searchIcon.classList.remove('active');
+         searchInput.classList.remove('active');
+         active = false;
+      }
+   });
+
+   searchInput.addEventListener('keydown', (event) => {
+      if (event.keyCode === 27) {
+         searchBar.classList.remove('active');
+         searchIcon.classList.remove('active');
+         searchInput.classList.remove('active');
+         active = false;
+      }
+   });
+
+
+   let errorDomString1 = '<div class="error-message">Please do not forget to enter a name</div>';
+   let errorDomString2 = '<div class="noResults" style="color:rgb(158,3,3); line-height:150%; font-size:80%">SORRY WE DON\'T RECOGNIZE THIS STUDENT \n PLEASE TRY ANOTHER NAME</div > ';
+
+
+   /*====================
+      FILTER FUNCTION...
+   /*====================*/
+   function filterList() {
+      // Declare variables
+      let filter, i, name, txtValue;
+      filter = searchInput.value.toUpperCase();
+      const errorDiv = document.createElement('div');
+      // const children = [];
+
+      // Loop through all list items, and hide those who don't match the search query
+      for (i = 0; i < eachStudentItem.length; i++) {
+         name = eachStudentItem[i].getElementsByTagName("h3")[0];
+         txtValue = name.textContent || eachStudentItem.innerText;
+
+         // when an index does not exist the program recognizes it as -1,
+         // so this condition gauges whether the value of the user's input
+         // is equal to any of the indexes in the current object/array/list of students
+         // $ if the value which in this case is the student's name than the code is executed
+         // the following code toggles between the student's profile being displayed on DOM or not
+         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            eachStudentItem[i].style.display = "";
+            // errorDiv.style.display = "";
+         } else {
+            eachStudentItem[i].style.display = 'none';
+            // errorDiv.innerHTML = errorDomString2;
+            // searchBar.appendChild(errorDiv.firstChild);
+            // alert('no results found ');
+         }
+      }
+      // =====================
+      // USER INPUT IS MISSING
+      // =====================
+      if (filter == "") {
+         errorDiv.innerHTML = errorDomString1;
+         searchBar.appendChild(errorDiv.firstChild);
+      }
+      else {
+         // REMOVE ERROR Message
+         const removeErrorString = document.querySelector("#search > div");
+         removeErrorString.style.display = 'none';
+         return errorDiv;
+      }
+   }
+
+   /*=====================================================
+     SEARCH BAR INPUT CAPTURED HERE VIA RETURN/ENTER KEY...
+   /*=====================================================*/
+
+   // executing our desired function when the user releases a key (return || enter) on the keyboard
+   searchInput.addEventListener("keydown", (event) => {
+      // Number 13 is the "Enter" key on the keyboard
+      if (event.keyCode === 13) {
+         // Cancel the default action, if needed
+         event.preventDefault();
+         // capture the value of the input and trigger that input to be logged to the console for testing purposes
+         //run filter function after user clicks return/enter key =" keydown"
+         filterList();
+         //if the user enters name after not entering a name than we remove the error message from DOM
+         // errorDiv.style.display = 'none';
+      }
+   });
+
+
+   /*====================
+      RETURN HOME FUNCTION...
+   /*====================*/
+   // declare the logo variable and store in it the access to the logo image on the DOM
+   const logo = document.querySelector("body > img");
+   // the logo now prepared for user to it at anytime and reset the current window's url to the home page
+   logo.addEventListener('click', () => {
+      window.location.href = '/Users/johnhenry/Desktop/techDegree/unit2/techdegree-project-2/index.html'
+   });
 
    console.log('All The DOM Content has load and you\'re ready to go!'); // <--- sanity check
 }); // <---- conclusion of DOMContentLoaded function
